8e7b3c50e59808dbce3bbb03659e41539f151c1f
kiranpki <kiranpki@sheridancollege.ca>
2020-01-29 19:18:01 -0500

    In-class 1

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,17 @@
+//@author: Kiranpreet Kaur
 public class Card {
-    
+
+    private static class Value1 {
+
+        public Value1() {
+        }
+    }
+
         public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
         public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
         private final Suit suit;
         private final Value value;
+        private final Value1 jocker=null;
         
         public Card(Suit s, Value gVal)
         {
@@ -14,7 +22,7 @@ public class Card {
 		return this.value;
 	}
 
-	
+
 	public Suit getSuit() {
 		return this.suit;
         }
